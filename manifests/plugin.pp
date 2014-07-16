01aca8f7a581307e417a3c63ef2fb4973f752e20
tel <tel@tels-macbook.local>
2014-07-16 18:26:44 -0400

    bugfix

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -6,6 +6,7 @@
 define eclipse::plugin ($name, $repo, $eclipse_dir='eclipse') {
   require eclipse
   package {$name:
+    name => $name,
     provider => 'eclipse_p2',
     install_options => [{'eclipse_dir' => $eclipse_dir }, {'repo' => $repo}]
   }
