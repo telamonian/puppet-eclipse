2b2765e2cdf3cbcff5e6266770da6027a4e58068
tel <tel@tels-macbook.local>
2014-07-16 01:24:30 -0400

    bugfix

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,10 +3,10 @@
 # Examples
 #
 #   include eclipse
-class eclipse ($eclipse_dir='eclipse') {
+class eclipse ($eclipse_dir='eclipse', $eclipse_exec_src='http://download.eclipse.org/eclipse/downloads/drops4/R-4.4-201406061215/eclipse-platform-4.4-macosx-cocoa-x86_64.tar.gz') {
   package {'eclipse':
     provider => 'compressed_dir',
     install_options => [{'eclipse_dir' => $eclipse_dir }],
-    source   => 'http://download.eclipse.org/eclipse/downloads/drops4/R-4.4-201406061215/eclipse-platform-4.4-macosx-cocoa-x86_64.tar.gz'
+    source   => $eclipse_exec_src
   }
 }
