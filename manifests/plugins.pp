0b49b125cdecb9c216137bf94842320749d50095
tel <tel@tels-macbook.local>
2014-07-16 01:40:16 -0400

    bugfix

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -5,5 +5,7 @@
 #   include eclipse::plugins
 class eclipse::plugins ($plugin_info) {
   require eclipse
+  notify{"plugin_info is: {$plugin_info}":}
+  debug($plugin_info)
   create_resources(classroom::plugin, $plugin_info)
 }
